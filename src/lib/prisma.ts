6782d9a0862055ef0ef78ce8f6d5408210df0945
Otthon Leão <otthonleao@hotmail.com>
2025-07-21 20:54:41 -0400

    feat: add migrations

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "@prisma/client"
+import { PrismaClient } from "../generated/prisma/client"
 
 // const globalForPrisma = globalThis as unknown as { prisma: PrismaClient }
 
@@ -11,7 +11,7 @@ let prisma: PrismaClient;
 if (process.env.NODE_ENV === 'production') {
   prisma = new PrismaClient();
 } else {
-	
+
   let globalWithPrisma = global as typeof globalThis & {
     prisma: PrismaClient;
   }
